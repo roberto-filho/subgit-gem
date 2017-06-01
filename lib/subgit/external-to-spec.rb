57158ec628b0f18890945fbd693f393a35416b38
Roberto Filho <roberto.w.filho@gmail.com>
2017-05-31 22:17:42 -0300

    Estruturando classe SpecCreator

--- lib/subgit/external-to-spec.rb
+++ lib/subgit/external-to-spec.rb
@@ -1,43 +1,60 @@
-#!/usr/bin/env ruby
-
 require_relative 'svnexternal'
 
-EXTERNALS_ROOT_DIR = "~/svn/cooperalfa/Desenv-1.0.3"
+module Subgit
+  EXTERNALS_ROOT_DIR = "~/svn/cooperalfa/Desenv-1.0.3"
 
-projects = []
-# Resolve os projetos a serem externalizados
-ARGV.each do |project|
-  projects.push project
-end
+  class SpecCreator
 
-PROJECTS_TO_RESOLVE = projects
+    def initialize(root_dir, projects = [])
+      @root_dir = root_dir
+      @projects = projects
+    end
 
-# svn propget svn:externals -R
-def to_external(external_definition_line)
-  # Primeiro, divide a linha no espaço
-  external = external_definition_line.split
+    def format
+      # roda o comando no diretório
+      # TODO Colocar esse comando em outra classe, para poder mocar
+      externals_from_file = `cd #{@root_dir} && svn propget svn:externals -R`
+      # Quebra em linhas
+      externals = externals_from_file.split("\n").select { |i|
+        i.size > 1
+      }
 
-  # A segunda parte (de trás pra frente) é o "link" do repositório
-  # Então divide a URL para pegar a parte do branch
-  url = external[external.length-2].split "/"
-  # Extrai o branch da URL
-  branch = url[url.length-1]
-  return Subgit::SvnExternal.new external[external.length-1], branch
-end
+      all_externals = []
 
-def resolve_project?(cli_arg)
-  return !cli_arg.nil? && cli_arg == "*"  ;
-end
+      externals.each do |ext|
+        external = to_external(ext)
+        #puts external_info.to_array.join ' - '
+        if @projects.include? external.folder or @projects.empty?
+          all_externals.push external.to_property
+        end
+      end
+      # retorna os caras
+      all_externals
+    end
 
-# roda o comando no diretório
-externals_from_file = `cd #{EXTERNALS_ROOT_DIR} && svn propget svn:externals -R`
+    def print_formatted
+      all = format
+      puts all.join "\n"
+    end
 
-externals = externals_from_file.split("\n").select { |i|
-  i.size > 1
-}
+    private
 
-externals.each do |ext|
-  external = to_external ext
-  #puts external_info.to_array.join ' - '
-  puts external.to_property if PROJECTS_TO_RESOLVE.include? external.folder
+    def to_external(external_definition_line)
+      # Primeiro, divide a linha no espaço
+      external = external_definition_line.split
+
+      # A segunda parte (de trás pra frente) é o "link" do repositório
+      # Então divide a URL para pegar a parte do branch
+      url = external[external.length-2].split "/"
+      # Extrai o branch da URL
+      branch = url[url.length-1]
+      return Subgit::SvnExternal.new external[external.length-1], branch
+    end
+
+    def resolve_project?(cli_arg)
+      return !cli_arg.nil? && cli_arg == "*"  ;
+    end
+
+  end
+
 end
