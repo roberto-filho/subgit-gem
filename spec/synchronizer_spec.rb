c1e7b02d83b17770e56863613d0c5c901cf41523
Roberto Filho <roberto.w.filho@gmail.com>
2017-06-07 22:54:13 -0300

    Avancando nos testes.

--- spec/synchronizer_spec.rb
+++ spec/synchronizer_spec.rb
@@ -4,25 +4,30 @@ require 'subgit/specmaker'
 RSpec.describe Subgit::Synchronizer do
   it 'imprime com cor quando configurado' do
     allow_any_instance_of(Subgit::Command) \
-      .to receive(:get_externals) \
-      .and_return File.read('spec/resources/svn-externals-com-path.txt')
+      .to receive(:get_current_branch) \
+      .and_return 'master'
 
-    maker = Subgit::SpecMaker.new '/some/dir'
+    allow_any_instance_of(Subgit::Command) \
+      .to receive(:fetch_svn_revisions) do |_m, dir|
+        puts "Fetched svn revisions on dir '#{dir}'"
+      end
+    allow_any_instance_of(Subgit::Command).to receive(:merge) do |_m, d, b|
+      puts "Merged '#{d}' with branch '#{b}'"
+    end
 
-    resultado = []
-    resultado.push 'something=Something-1.10.1'
-    resultado.push 'another=Another-0.0.1'
+    syncer = Subgit::Synchronizer.new 'home/git/project', 'master'
 
-    expect(maker.format).to eq(resultado)
+    syncer.update_repo
+
+    expect_any_instance_of(String).to receive(:colorize)
   end
 
-  it 'imprime com cor quando configurado' do
+  it 'imprime sem cor quando configurado' do
     allow_any_instance_of(Subgit::Command) \
       .to receive(:get_current_branch) \
       .and_return 'master'
 
     allow_any_instance_of(Subgit::Command) \
-      .to receive(:fetch_svn_revisions) \
-      .and_do_nothing
+      .to receive(:fetch_svn_revisions)
   end
 end
